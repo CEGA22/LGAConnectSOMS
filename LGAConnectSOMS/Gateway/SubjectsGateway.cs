24f053b6f3d438ddf2291d87ac90275698b5572e
Cedric Gabrang <cedric@xam.com.au>
2022-01-09 19:09:43 +0800

    update

--- LGAConnectSOMS/Gateway/SubjectsGateway.cs
+++ LGAConnectSOMS/Gateway/SubjectsGateway.cs
@@ -11,7 +11,7 @@ namespace LGAConnectSOMS.Gateway
 {
     public class SubjectsGateway
     {
-        static string BaseUrl = "http://localhost:2195/api/lga/subjects";
+        static string BaseUrl = "http://cegagabrang-001-site1.btempurl.com/api/lga/subjects";
 
         public async Task<IEnumerable<Subjects>> GetSubjects()
         {
